8de41e6db615a4d2ae91eae66e72b843778215e2
Tejaswinigshet <49709735+Tejaswinigshet@users.noreply.github.com>
2019-06-09 18:57:19 +0530

    Update Track.java

--- src/main/java/com/stackroute/muzixservices/domain/Track.java
+++ src/main/java/com/stackroute/muzixservices/domain/Track.java
@@ -18,46 +18,4 @@ public class Track {
     private int trackID;
     private String trackName;
     private String trackComments;
-
-  /*public Track() {
-  }
-
-  public Track(int trackID, String trackName, String trackComments) {
-      this.trackID = trackID;
-      this.trackName = trackName;
-      this.trackComments = trackComments;
-  }
-
-  @Override
-  public String toString() {
-      return "Track{" +
-              "trackID=" + trackID +
-              ", trackName='" + trackName + '\'' +
-              ", trackComments='" + trackComments + '\'' +
-              '}';
-  }
-
-  public int getTrackID() {
-      return trackID;
-  }
-
-  public void setTrackID(int trackID) {
-      this.trackID = trackID;
-  }
-
-  public String getTrackName() {
-      return trackName;
-  }
-
-  public void setTrackName(String trackName) {
-      this.trackName = trackName;
-  }
-
-  public String getTrackComments() {
-      return trackComments;
-  }
-
-  public void setTrackComments(String trackComments) {
-      this.trackComments = trackComments;
-  }*/
 }
